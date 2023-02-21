00f16a928a2ca7939ee766cda7ede80dfc86e073
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2023-02-21 14:39:50 +0000

    added files

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const app = express();
 // One from env or port 3000
 const port = process.env.PORT || 3000;
 
-
 try {
   // try to connect to mysql database
-  const result = await sequelize.sync()
+  const result = await sequelize.sync();
   console.log(result);
 } catch (error) {
-console.log(error);
+  console.log(error);
 }
 
 app.use(bodyParser.json());
@@ -23,6 +22,9 @@ app.use(bodyParser.json());
 // NEED TO USE THE ROUTER
 app.use("/list", router);
 
+app.get("/", (req, res) => {
+  res.send("Hello Oliver from GCP");
+});
 
 app.listen(port, () => {
   console.log("Server is running on port" + port);
